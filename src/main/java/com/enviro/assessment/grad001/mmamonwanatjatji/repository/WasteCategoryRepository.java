628d91be0bbafe6400a7e7066f276434bd724ac9
M.M Tjatji <tjatjikm99@gmail.com>
2025-02-06 19:41:14 +0200

    Added a comment on  WasteCategoryRepository.java

--- src/main/java/com/enviro/assessment/grad001/mmamonwanatjatji/repository/WasteCategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/mmamonwanatjatji/repository/WasteCategoryRepository.java
@@ -3,6 +3,7 @@ package com.enviro.assessment.grad001.mmamonwanatjatji.repository;
 import com.enviro.assessment.grad001.mmamonwanatjatji.model.WasteCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Interface that extends JpaRepository to handle CRUD operations for WasteCategory
 public interface WasteCategoryRepository extends JpaRepository<WasteCategory, Long> {
     
 }
